9fd7840cafc63cca258d52e9d01c9b29cdd37ae9
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-03-28 11:29:47 +0200

    integration test config

--- config_integration_test.go
+++ config_integration_test.go
@@ -1,1 +1,42 @@
 package libwekan
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfig_MatchingBoardIsPresent(t *testing.T) {
+	ass := assert.New(t)
+
+	slugWhichMatch := "tableau-crp-" + t.Name()
+	boardID := BoardID(newId())
+	err := wekan.InsertBoard(ctx, Board{
+		ID:   boardID,
+		Slug: BoardSlug(slugWhichMatch),
+	})
+	ass.NoError(err)
+
+	config, err := wekan.SelectConfig(ctx)
+	ass.NoError(err)
+
+	_, ok := config.Boards[boardID]
+	ass.True(ok)
+}
+
+func TestConfig_NotMatchingBoardIsAbsent(t *testing.T) {
+	ass := assert.New(t)
+
+	slugDontMatch := "tableau-pascrp-" + t.Name()
+	boardID := BoardID(newId())
+	err := wekan.InsertBoard(ctx, Board{
+		ID:   boardID,
+		Slug: BoardSlug(slugDontMatch),
+	})
+	ass.NoError(err)
+
+	config, err := wekan.SelectConfig(ctx)
+	ass.NoError(err)
+
+	_, ok := config.Boards[boardID]
+	ass.False(ok)
+}
